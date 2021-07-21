e140cf632d3f280be3c3c9b6e79a3dc84bca3c4b
Sandro Keil <github@sandro-keil.de>
2021-07-21 08:54:33 +0200

    Add support for role and hotSpot type

---
+++ src/Metadata/InspectioJson/RoleMetadata.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @see       https://github.com/event-engine/php-code-generator-event-engine-ast for the canonical source repository
+ * @copyright https://github.com/event-engine/php-code-generator-event-engine-ast/blob/master/COPYRIGHT.md
+ * @license   https://github.com/event-engine/php-code-generator-event-engine-ast/blob/master/LICENSE.md MIT License
+ */
+
+declare(strict_types=1);
+
+namespace EventEngine\CodeGenerator\EventEngineAst\Metadata\InspectioJson;
+
+final class RoleMetadata implements \EventEngine\CodeGenerator\EventEngineAst\Metadata\RoleMetadata
+{
+    use JsonMetadataTrait;
+
+    private function __construct()
+    {
+    }
+
+    public function customData(): array
+    {
+        return $this->customData;
+    }
+}
